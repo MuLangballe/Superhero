522c77f19b932a54de31d6cc5c21cfe24448f77c
NikoP93 <nipe0001@stud.kea.dk>
2023-11-05 17:11:37 +0100

    Edit rykket til UI og Controller lavet

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -6,7 +6,6 @@ public class Database {
 
     ArrayList<Superhero> superheroes = new ArrayList<>();
 
-
     public void createSuperhero(String name, String realName, String superPower, int yearCreated, boolean isHuman, int strength) {
         superheroes.add(new Superhero(name, realName, superPower, yearCreated, isHuman, strength));
     }
