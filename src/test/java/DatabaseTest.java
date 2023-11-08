eb2e2408571f463ed90af1ba3ba097fdeaa4b450
theaskou <thsk0001@stud.kea.dk>
2023-11-08 11:42:55 +0100

    Changes by nikol

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -3,6 +3,11 @@ import domain_model.Superhero;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -12,13 +17,70 @@ class DatabaseTest {
     Database db;
     Scanner keyboard;
 
-    ArrayList<Superhero> testSuperheroes = new ArrayList<>();
+
+
+    File testfile = new File("TestSuperhero.csv");
+
+    public ArrayList<Superhero> loadSuperheroTest(){
+        ArrayList<Superhero> testSuperheroes = new ArrayList<>();
+        Scanner keyboard;
+
+            try {
+                keyboard = new Scanner(testfile, StandardCharsets.ISO_8859_1);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Superhero superhero;
+            while (keyboard.hasNext()){
+                String line = keyboard.nextLine();
+                String[] parametre = line.split(";");
+                String name = parametre[0];
+                String realName = parametre[1];
+                String superPowers = parametre[2];
+                String yearCreated = parametre[3];
+                String isHuman = parametre[4];
+                String strength = parametre[5];
+                superhero = new Superhero(
+                        name,
+                        realName,
+                        superPowers,
+                        Integer.parseInt(yearCreated),
+                        Boolean.parseBoolean(isHuman),
+                        Integer.parseInt(strength)
+                );
+
+                testSuperheroes.add(superhero);
+            }
+            keyboard.close();
+            return testSuperheroes;
+
+
+    }
+
+    public void saveListOfTestSuperheroes(ArrayList<Superhero> testsuperheroes){
+        try(PrintStream output = new PrintStream(testfile)){
+            for (Superhero superhero : testsuperheroes) {
+                output.println(superhero.getName() + ";"
+                        + superhero.getRealName() + ";"
+                        + superhero.getSuperPower() + ";"
+                        + superhero.getYearCreated() + ";"
+                        + superhero.isHuman() + ";"
+                        + superhero.getStrength()
+                );
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 
 
 
     @BeforeEach
     void setUp(){
         db = new Database();
+        ArrayList<Superhero> testSuperheroes = new ArrayList<>();
         keyboard = new Scanner(System.in);
         Superhero s1 = new Superhero("Spin", "Miles", "Arachnosting", 2020, true, 1770);
         Superhero s2 = new Superhero("Superman", "Clark", "Styrke", 1940, false, 2330);
@@ -28,6 +90,7 @@ class DatabaseTest {
         testSuperheroes.add(s2);
         testSuperheroes.add(s3);
         testSuperheroes.add(s4);
+        saveListOfTestSuperheroes(testSuperheroes);
     }
     @Test
     void createSuperhero() {
